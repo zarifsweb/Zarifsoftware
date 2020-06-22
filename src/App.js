03355aa1e2882a54d2b0f05ba6b136b3d2f7c24c
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-22 21:54:25 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Login from './Components/Login';
+import Signup from './Components/Signup';
 import Footer from './Components/Footer';
 import './App.css';
 
@@ -28,6 +29,9 @@ function App() {
             </Route>
             <Route path="/login">
               <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
             </Route>
           </Switch>
          </div>
