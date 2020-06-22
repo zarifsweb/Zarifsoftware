a5dd376f86c875e8914dca09dfa20c31ee48d0d3
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-22 21:20:07 +0600

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -23,11 +23,11 @@ class Login extends Component {
   <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3">
       <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-        Your Name
+        Email
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Your name" value={this.state.email} name="email" onChange={this.handleChange}/>
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Enter your Email" value={this.state.email} name="email" onChange={this.handleChange}/>
     </div>
   </div>
   <div class="md:flex md:items-center mb-6">
@@ -37,7 +37,7 @@ class Login extends Component {
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-username" type="password" placeholder="Your Password" value={this.state.password} name="password" onChange={this.handleChange}/>
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-username" type="password" placeholder="Enter your Password" value={this.state.password} name="password" onChange={this.handleChange}/>
     </div>
   </div>
   <div class="md:flex md:items-center mb-6">
