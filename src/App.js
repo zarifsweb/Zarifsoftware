ca7f5f8e4f78c1b93fbbc1771e16952b8e31dcfa
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-22 18:31:26 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Footer from './Components/Footer';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
+     <Router>    
       <Navbar/>
          <hr/>
          <div className="page">
-            <Home className="home"/>
-            <hr/>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
          </div>
          <hr/>
       <Footer/>
+     </Router>   
     </div>
   );
 }
