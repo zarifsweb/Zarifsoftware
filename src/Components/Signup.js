9d7dbe63bde6cf2ba325a9d8abbe027f9852551c
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-23 05:37:10 +0600

    Update Signup.js

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -23,7 +23,7 @@ class Signup extends Component {
             <form class="w-full max-w-sm container">
   <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+      <label class="block text-gray-200 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
         Username
       </label>
     </div>
@@ -33,7 +33,7 @@ class Signup extends Component {
   </div>
   <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-email">
+      <label class="block text-gray-200 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-email">
         Email
       </label>
     </div>
@@ -43,7 +43,7 @@ class Signup extends Component {
   </div>
   <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-username">
+      <label class="block text-gray-200 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-username">
         Password
       </label>
     </div>
@@ -53,7 +53,7 @@ class Signup extends Component {
   </div>
   <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3"></div>
-    <label class="md:w-2/3 block text-gray-500 font-bold">
+    <label class="md:w-2/3 block text-gray-200 font-bold">
       <input class="mr-2 leading-tight" type="checkbox"/>
       <span class="text-sm">
         Remember me
