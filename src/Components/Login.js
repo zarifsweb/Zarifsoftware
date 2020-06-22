b7f2d5b7a4148c1ed6f21d4b79deac3868e1e3e3
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-23 05:34:06 +0600

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -19,7 +19,7 @@ class Login extends Component {
   }
     render(){
         return (
-            <form class="w-full max-w-sm">
+            <form class="w-full max-w-sm container">
   <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3">
       <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
