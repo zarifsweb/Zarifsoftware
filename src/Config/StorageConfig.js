d9e922c890a8235ac7b89ad8eba5e87254b1a92f
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-22 20:02:35 +0600

    Update StorageConfig.js

--- src/Config/StorageConfig.js
+++ src/Config/StorageConfig.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/storage";
 
 const DB_CONFIG = {
-  apiKey: "AIzaSyCffYkllsCTgVlsDDGmKyFiByduN0rDRjA",
-  authDomain: "nayeems-english-care.firebaseapp.com",
-  databaseURL: "https://nayeems-english-care.firebaseio.com",
-  projectId: "nayeems-english-care",
-  storageBucket: "nayeems-english-care.appspot.com",
-  messagingSenderId: "240472520672",
-  appId: "1:240472520672:web:0667c64868fb619ce01d85",
+    apiKey: "AIzaSyDXMFsk0lAOQ_xAZIkfhc9uheoVKZT5s88",
+    authDomain: "zarif-software.firebaseapp.com",
+    databaseURL: "https://zarif-software.firebaseio.com",
+    projectId: "zarif-software",
+    storageBucket: "zarif-software.appspot.com",
+    messagingSenderId: "38199309727",
+    appId: "1:38199309727:web:fa3c0cb82e81050860b9aa"
 };
 
 firebase.initializeApp(DB_CONFIG);
