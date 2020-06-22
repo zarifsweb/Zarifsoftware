f9d045c8edc39ebc5c44276c813b5ddf470110e4
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-22 20:57:23 +0600

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,18 +1,30 @@
 import React, {Component} from 'react';
 
 class Login extends Component {
-  
+    constructor(props){
+        this.onChange = this.onChange.bind(this);
+        this.state = {
+            email : "",
+            password : "",
+        };
+    }
+ 
+    onChange(e){
+       this.setState({
+          [e.target.name] : e.target.value,
+       })
+    }
     render(){
         return (
             <form class="w-full max-w-sm">
   <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3">
       <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-        Full Name
+        Your Name
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Sadman Islam Zarif"/>
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Sadman Islam Zarif" value={this.state.email} onChange={this.onChange}/>
     </div>
   </div>
   <div class="md:flex md:items-center mb-6">
@@ -22,7 +34,7 @@ class Login extends Component {
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-username" type="password" placeholder="******************"/>
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-username" type="password" placeholder="******************" value={this.state.password} onChange={this.onChange}/>
     </div>
   </div>
   <div class="md:flex md:items-center mb-6">
