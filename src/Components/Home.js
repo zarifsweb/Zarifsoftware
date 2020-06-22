258253f8b0da4ecbbef10d5804070a65fbb91ad9
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-22 17:32:44 +0600

    Add files via upload

---
+++ src/Components/Home.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import Logo from './my_image.jpg';
+
+class Home extends Component {
+  
+    render(){
+        return (
+            <section class="text-gray-700 body-font">
+  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+      <img class="object-cover object-center rounded" alt="hero" src={Logo}/>
+    </div>
+    <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Welcome to 
+        <br class="hidden lg:inline-block"/> ZarifSoftware
+      </h1>
+      <p class="mb-8 leading-relaxed">Here you can learn coding from blog. You can learn any programming language do you want. Plus you can learn to make cool projects with source code. I am Zarif. I am a web and android developer. Javascript is my favourite programming language.</p>
+      <div class="flex justify-center">
+        <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Signup</button>
+        <button class="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">Login</button>
+      </div>
+    </div>
+  </div>
+</section>
+        )
+    } 
+  }
+
+export default Home;
+
+
