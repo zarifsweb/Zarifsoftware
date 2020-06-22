d467040eb134e624d44051dbf3ce52691e03b8f6
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-22 18:31:43 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Footer from './Components/Footer';
