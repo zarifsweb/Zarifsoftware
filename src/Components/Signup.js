436f9f0936aeae27b076ad93a7a02dd65d357e71
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-23 06:10:39 +0600

    Update Signup.js

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -81,7 +81,7 @@ class Signup extends Component {
   <div class="md:flex md:items-center">
     <div class="md:w-1/3"></div>
     <div class="md:w-2/3">
-      <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+      <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button" onClick={this.onSubmit}>
         Signup
       </button>
     </div>
