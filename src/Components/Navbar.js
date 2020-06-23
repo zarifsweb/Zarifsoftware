3099a877086302823a43d433b5eecf1f472db14a
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-23 17:55:12 +0600

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import checkCookie from './cookies/checkCookie';
 import Navbar1 from './Navbar1';
+import Navbar2 from './Navbar2';
 
 class Navbar extends Component {
 
     render(){
         return (
-            <Navbar1/>
+            <div>
+              {checkCookie("user") ?
+                   (<Navbar2/>)
+              :
+                   (<Navbar1/>)
+        
+              }
+            </div>
         )
     } 
   }
