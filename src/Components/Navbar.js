f223d9ae32949645386b9a0cfc3f9c1eddd1c19f
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-23 17:50:34 +0600

    Create Navbar.js

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Navbar1 from './Navbar1';
+
+class Navbar extends Component {
+
+    render(){
+        return (
+            <Navbar1/>
+        )
+    } 
+  }
+
+export default Navbar;
