b591cc00e921cac037bf3222d1cdf2fc156cb3cb
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-23 21:54:15 +0600

    Create recent.js

---
+++ src/Components/recent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class recent extends Component {
+  
+    render(){
+        return (
+            <h1 align="center" style={{fontSize: '30px'}}>Recent Posts</h1>
+        )
+    } 
+  }
+
+export default recent;
+
+
