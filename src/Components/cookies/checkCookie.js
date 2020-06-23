c946865847b0b23ad3102f45beeb89ee29cfadec
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-23 16:16:47 +0600

    Update checkCookie.js

--- src/Components/cookies/checkCookie.js
+++ src/Components/cookies/checkCookie.js
@@ -1,1 +1,12 @@
+import getCookie from "./getCookie";
 
+function checkCookie(cname) {
+  var user = getCookie(cname);
+  if (user !== "") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export default checkCookie;
