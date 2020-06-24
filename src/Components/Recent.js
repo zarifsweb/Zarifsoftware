bb4ab6414025273cafd3ff57693688580da19cd3
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-24 08:19:30 +0600

    Update Recent.js

--- src/Components/Recent.js
+++ src/Components/Recent.js
@@ -4,31 +4,33 @@ import firebase from '../Config/Config';
 class Recent extends Component {
     constructor(props){
         super(props);
+        this.subscribe = this.subscribe.bind(this);
         this.ref = firebase.firestore();
         this.state = {
            categories: []
         }
 
     }
-    componentWillMount(){
-       const categories = []; 
-       this.ref.collections('categories').get()
-       .then((snap)=>{
-           snap.forEach((doc)=>{
-              const {name, description, url} = doc.data()
-              categories.push({
-                 name,
-                 description,
-                 url
-              })
-           });
-           this.setState({categories : categories})
-           
-       })
+    componentWillMount(){ 
+       this.ref.collection('categories').get()
+       .then(this.subscribe)
        .catch((error)=>{
           console.log(error);
        })
        
+    }
+    subscribe(snap){
+       const categories = [];
+       snap.forEach((doc)=>{
+          const {name, description, url} = doc.data()
+          categories.push({
+              name,
+              description,
+              url
+           })
+        });
+        this.setState({categories : categories})
+
     }
     render(){
         return (
