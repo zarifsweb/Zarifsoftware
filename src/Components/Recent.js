37da16ec6d01a9b0dbb197e48e5e15107d959577
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-24 07:50:02 +0600

    Update Recent.js

--- src/Components/Recent.js
+++ src/Components/Recent.js
@@ -4,7 +4,7 @@ import firebase from '../Config/Config';
 class Recent extends Component {
     constructor(props){
         super(props);
-        this.ref = firebase.firestore().collection('categories');
+        this.ref = firebase.firestore();
         this.state = {
            categories: []
         }
@@ -12,9 +12,9 @@ class Recent extends Component {
     }
     componentWillMount(){
        const categories = []; 
-       this.ref.limit(2).get()
-       .then((data)=>{
-           data.forEach((doc)=>{
+       this.ref.collections('categories').get()
+       .then((snap)=>{
+           snap.forEach((doc)=>{
               const {name, description, url} = doc.data()
               categories.push({
                  name,
@@ -35,8 +35,8 @@ class Recent extends Component {
 <section class="text-gray-700 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-wrap -m-4">
-     {this.state.categories.map((category)=>{
-      return (<div class="xl:w-1/4 md:w-1/2 p-4">
+     {this.state.categories.map(category =>
+      <div class="xl:w-1/4 md:w-1/2 p-4">
         <div class="bg-gray-100 p-6 rounded-lg">
           <img class="h-40 rounded w-full object-cover object-center mb-6" src={category.url} alt="content"/>
           <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
@@ -44,7 +44,7 @@ class Recent extends Component {
           <p class="leading-relaxed text-base">{category.description}</p>
         </div>
       </div>)
-     })}
+     }
      </div>
   </div>
 </section>
