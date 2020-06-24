6ccdaf282f8172a34aa140be2b01f7c35b6016da
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-24 09:14:59 +0600

    Update Categories.js

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -4,13 +4,13 @@ class Categories extends Component {
   
     render(){
         return (
-<section class="text-gray-700 body-font overflow-hidden" style={{backgroundColor: "white"}}>
+<section class="text-gray-700 body-font overflow-hidden">
   <div class="container px-5 py-24 mx-auto">
     <div class="-my-8">
       <div class="py-8 flex flex-wrap md:flex-no-wrap">
         <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
           <span class="tracking-widest font-medium title-font text-gray-900">CATEGORY</span>
-          <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+          <span class="mt-1 text-gray-300 text-sm">12 Jun 2019</span>
         </div>
         <div class="md:flex-grow">
           <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Python</h2>
@@ -26,7 +26,7 @@ class Categories extends Component {
       <div class="py-8 flex border-t-2 border-gray-200 flex-wrap md:flex-no-wrap">
         <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
           <span class="tracking-widest font-medium title-font text-gray-900">CATEGORY</span>
-          <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+          <span class="mt-1 text-gray-300 text-sm">12 Jun 2019</span>
         </div>
         <div class="md:flex-grow">
           <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Javascript</h2>
@@ -42,7 +42,7 @@ class Categories extends Component {
       <div class="py-8 flex border-t-2 border-gray-200 flex-wrap md:flex-no-wrap">
         <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
           <span class="tracking-widest font-medium title-font text-gray-900">CATEGORY</span>
-          <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+          <span class="mt-1 text-gray-300 text-sm">12 Jun 2019</span>
         </div>
         <div class="md:flex-grow">
           <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Java</h2>
