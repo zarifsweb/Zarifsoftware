64991e8ec3e6e7f43f4a8eaf85db78f028f343a9
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-24 09:59:15 +0600

    Update Categories.js

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -15,6 +15,19 @@ class Categories extends Component {
     }
     
     componentWillMount(){
+       const categories = []
+       this.db.collection("categories").get().then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+             console.log(`${doc.id} => ${doc.data()}`);
+             categories.push({
+                id: doc.id,
+                name: doc.data().name,
+                description : doc.data().description,
+                url: doc.data().url,
+             })
+          });
+          this.setState({categories});
+       });
        if(checkCookie("user")){
           this.setState({login: true})
        }else{
@@ -30,11 +43,11 @@ class Categories extends Component {
 <section class="text-gray-700 body-font overflow-hidden">
   <div class="container px-5 py-24 mx-auto">
     <div class="-my-8">
-
+    { this.state.categories.map ( category => 
       <div class="py-8 flex flex-wrap md:flex-no-wrap p-8" style={{backgroundColor: "white"}}>
         <div class="md:flex-grow">
-          <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Python</h2>
-          <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+          <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">{category.name}</h2>
+          <p class="leading-relaxed">{category.description}</p>
           <a href="/" class="text-indigo-500 inline-flex items-center mt-4">Learn More
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
@@ -44,7 +57,7 @@ class Categories extends Component {
         </div>
       </div>
       <hr/>
- 
+     )}
     </div>
   </div>
 </section>   
