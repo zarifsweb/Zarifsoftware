0a9054abe624a92049f6639ff358d8fb40ab88d8
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-24 12:13:20 +0600

    Update Categories.js

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -50,7 +50,7 @@ class Categories extends Component {
       <div class="py-8 flex flex-wrap md:flex-no-wrap p-8" style={{backgroundColor: "white"}}>
         <div class="md:flex-grow">
           <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">{category.name}</h2>
-          <p class="leading-relaxed">{category.description.substring(0, 70)}..</p>
+          <p class="leading-relaxed">{category.description.substring(0, 100)}..</p>
           <a href="/" class="text-indigo-500 inline-flex items-center mt-4">Learn More
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
