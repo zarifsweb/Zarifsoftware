7e59e74cf42d94343f1378389bf3ab5694f52678
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-24 09:29:52 +0600

    Update Categories.js

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -8,7 +8,7 @@ class Categories extends Component {
   <div class="container px-5 py-24 mx-auto">
     <div class="-my-8">
 
-      <div class="py-8 flex flex-wrap md:flex-no-wrap" style={{backgroundColor: "white"}}>
+      <div class="py-8 flex flex-wrap md:flex-no-wrap p-8" style={{backgroundColor: "white"}}>
         <div class="md:flex-grow">
           <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Python</h2>
           <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
