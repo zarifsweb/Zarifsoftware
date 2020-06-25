be3d1c743e8a1e49c70c60e2c6b149eb4cd006aa
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-25 13:28:41 +0600

    Update Category.js

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -20,7 +20,7 @@ class Category extends Component {
         firebase
           .firestore()
           .collection('cat')
-          .doc("Ai97ST3yjuTcjvU60M6G")
+          .doc(this.props.match.params.id)
           .get()
           .then((doc) => {
             if (doc.exists){
