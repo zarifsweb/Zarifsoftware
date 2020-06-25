43d94270d3af873d771478ad3281cc4ae501e2a1
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-25 19:40:39 +0600

    Update Categorydisplay.js

--- src/Components/Categorydisplay.js
+++ src/Components/Categorydisplay.js
@@ -12,10 +12,10 @@ class Categorydisplay extends Component {
         return (
 <section class="text-gray-700 body-font">
   <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={this.url}/>
+    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="{this.url}"/>
     <div class="text-center lg:w-2/3 w-full">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">{this.name}</h1>
-      <p class="mb-8 leading-relaxed">{this.description}</p>
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">this.name</h1>
+      <p class="mb-8 leading-relaxed">this.description</p>
     </div>
   </div>
 </section>
