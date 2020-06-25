d6ce0478c358b54ef75c0ef2953e3464f74c427e
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-25 07:52:03 +0600

    Update Category.js

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -3,11 +3,45 @@ import Categorydisplay from './Categorydisplay';
 import Categoryblog from './Categoryblog';
 
 class Category extends Component {
-  
+    constructor(props){
+        super(props);
+        this.state ={
+           name : "",
+           descriprion : "",
+           url : "",
+           login: false,
+           error: ""
+           
+        }
+    }
+    
+    componentWillMount(){     
+        firebase
+          .firestore()
+          .collection('cat')
+          .doc(this.props.match.params.id)
+          .get()
+          .then((doc) => {
+            if (doc.exists()){
+               this.setState({
+                  name: doc.data().name,
+                  description: doc.data().description,
+                  url: doc.data().url
+               })
+            }
+          })
+          .catch((err) => console.error(err)); 
+
+       if(checkCookie("user")){
+          this.setState({login: true})
+       }else{
+          this.setState({login: false})
+       }
+    }
     render(){
         return (
-           <Categorydisplay/>
-           <Categoryblog/>
+           <Categorydisplay name={this.state.name} description={this.state.description} url={this.state.url}/>
+           <Categoryblog name={this.state.name}/>
         )
     } 
   }
