ee20702ea3f38e6348b5155eab891492dbc9a84d
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-25 08:37:23 +0600

    Update Categories.js

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -54,7 +54,7 @@ class Categories extends Component {
         <div class="md:flex-grow">
           <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">{category.data.name}</h2>
           <p class="leading-relaxed">{category.data.description.substring(0, 100)}..</p>
-          <a href={`/category/${this.state.id}`} class="text-indigo-500 inline-flex items-center mt-4">Learn More
+          <a href={`/category/${category.id}`} class="text-indigo-500 inline-flex items-center mt-4">Learn More
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
